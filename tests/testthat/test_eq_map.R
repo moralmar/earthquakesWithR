7e674bfeff8c29f52be360ef03c30899317e7762
moralmar <mmorales@gmx.ch>
2017-07-19 17:52:24 +0200

    c19/ added testthat

--- tests/testthat/test_eq_map.R
+++ tests/testthat/test_eq_map.R
@@ -9,23 +9,22 @@
 ## created: 19.07.2017                                last update: 19.07.2017 ##
 ################# ~~~~~~~~~~~~~~~~~ ######## ~~~~~~~~~~~~~~~~~ #################
 
-context("GEOM Timeline")
-
+context("Leaflet Map Checking")
 
 df_clean <- eq_clean_data()
-
-obj_out <- df_clean %>% dplyr::filter(COUNTRY %in% c('USA', 'PERU')) %>%
-        dplyr::filter(date > '2000-01-01') %>%
-        ggplot() +
-        geom_timeline(aes(x = date, y = COUNTRY, color = TOTAL_DEATHS,
-                          size = EQ_PRIMARY))
+obj_out <- df_clean %>%
+                dplyr::filter(COUNTRY == 'JAPAN') %>%
+                dplyr::filter(lubridate::year(date) >= 2000)
 
 
-test_that("MMORALES: Is GEOM Timeline function giving back ggplot object (??)", {
+
+test_that('MMORALES: Is the MAP correct Object (??)', {
+
+        map <- eq_map(obj_out, annot_col = 'date')
 
 
-        expect_s3_class(obj_out, 'ggplot')
-
+        expect_s3_class(map, 'leaflet')
+        expect_s3_class(map, 'htmlwidget')
 })
 
 
